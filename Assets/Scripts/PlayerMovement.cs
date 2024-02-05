454a3932661349f3ee65314b748f296f0f72d0d4
Jenil Sanjaykumar Pandya <jenil.190410107054@gmail.com>
2024-02-04 22:17:16 -0500

    multiplayer working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,39 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-
+    PhotonView view;
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody>();    
+        rb=GetComponent<Rigidbody>();
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (view.IsMine) 
         {
-            rb.velocity= new Vector3(0,5,0);
-        }
-        if (Input.GetKeyDown("up"))
-        {
-            rb.velocity= new Vector3(0,0,5);
-        }
-        if (Input.GetKeyDown("down"))
-        {
-            rb.velocity= new Vector3(0,0,-5);
-        }
-        if (Input.GetKeyDown("right"))
-        {
-            rb.velocity= new Vector3(5,0,0);
-        }
-        if (Input.GetKeyDown("left"))
-        {
-            rb.velocity= new Vector3(-5,0,0);
+            if (Input.GetKeyDown("space"))
+            {
+                rb.velocity= new Vector3(0,5,0);
+            }
+            if (Input.GetKeyDown("up"))
+            {
+                rb.velocity= new Vector3(0,0,5);
+            }
+            if (Input.GetKeyDown("down"))
+            {
+                rb.velocity= new Vector3(0,0,-5);
+            }
+            if (Input.GetKeyDown("right"))
+            {
+                rb.velocity= new Vector3(5,0,0);
+            }
+            if (Input.GetKeyDown("left"))
+            {
+                rb.velocity= new Vector3(-5,0,0);
+            }
         }
     }
 }
