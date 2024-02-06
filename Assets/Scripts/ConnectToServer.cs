814cfd5fd0d07dc6719a1bc5c5b4bf362d8609ad
Jenil Sanjaykumar Pandya <jenil.190410107054@gmail.com>
2024-02-06 09:46:07 -0500

    Pre game Lobby

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement; 
 
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    public InputField usernameInput;
+    public Text buttonText;
+
     // Start is called before the first frame update
-    void Start()
+    
+    // void Start()
+    // {
+    //     PhotonNetwork.ConnectUsingSettings();
+    // }
+    public void onClickConnect()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if (usernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = usernameInput.text;
+            buttonText.text = " Connecting....";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
     // Update is called once per frame
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.JoinLobby();
-    }
-    public override void OnJoinedLobby()
-    {
+        //PhotonNetwork.JoinLobby();
         SceneManager.LoadScene("Lobby");
     }
+    // public override void OnJoinedLobby()
+    // {
+    //     SceneManager.LoadScene("Lobby");
+    // }
 }
