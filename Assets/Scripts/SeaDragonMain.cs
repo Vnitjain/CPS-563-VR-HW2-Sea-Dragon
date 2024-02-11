3036dba7ef1ff91b1d609fb438c3bad3f039374e
Jenil Sanjaykumar Pandya <jenil.190410107054@gmail.com>
2024-02-11 17:07:16 -0500

    Multiplayer Test

--- Assets/Scripts/SeaDragonMain.cs
+++ Assets/Scripts/SeaDragonMain.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 
 //photon id : caded358-3968-4e21-957b-4e6d80e49f95
 public class SeaDragonMain : MonoBehaviour
@@ -14,50 +14,60 @@ public class SeaDragonMain : MonoBehaviour
     private bool jumpFlag;
     public static float verticalInput;
     public static float horizontalInput;
+    PhotonView view;
     void Start()
     {
         dragonRigidBodyObject = GetComponent<Rigidbody>();
         isJumpKeyPressed = false;
         jumpFlag = true;
+        view = GetComponent<PhotonView>();
     }
     void Update()
-    {
-        if (Input.GetButton("Jump") && jumpFlag)
+    {   if(view.IsMine)
         {
-            isJumpKeyPressed = true;
-            jumpFlag = false;
-            isGrounded = false;
+            if (Input.GetButton("Jump") && jumpFlag)
+            {
+                isJumpKeyPressed = true;
+                jumpFlag = false;
+                isGrounded = false;
+            }
+            if (Input.GetButton("Fire1"))
+            {
+                isFire1Pressed = true;
+            }
+            verticalInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal");
         }
-        if (Input.GetButton("Fire1"))
-        {
-            isFire1Pressed = true;
-        }
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
     }
     void FixedUpdate()
     {
-        dragonRigidBodyObject.velocity = new Vector3(horizontalInput * 10, dragonRigidBodyObject.velocity.y, verticalInput * 10);
-        if (isJumpKeyPressed)
+        if(view.IsMine)
         {
-            dragonRigidBodyObject.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
-            isJumpKeyPressed = false;
-        }
-        if (isFire1Pressed)
-        {
-            isFire1Pressed = false;
+            dragonRigidBodyObject.velocity = new Vector3(horizontalInput * 10, dragonRigidBodyObject.velocity.y, verticalInput * 10);
+            if (isJumpKeyPressed)
+            {
+                dragonRigidBodyObject.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+                isJumpKeyPressed = false;
+            }
+            if (isFire1Pressed)
+            {
+                isFire1Pressed = false;
+            }
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == 3)
+        if(view.IsMine)
         {
-            jumpFlag = true;
-            isGrounded = true;
-        }
-        if (collision.gameObject.layer == 7)
-        {
-            Destroy(collision.gameObject);
+            if (collision.gameObject.layer == 3)
+            {
+                jumpFlag = true;
+                isGrounded = true;
+            }
+            if (collision.gameObject.layer == 7)
+            {
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
