8ee03ed542c5d4361915a3dc039ed23165f6df5e
ypratham07 <35989325+ypratham07@users.noreply.github.com>
2024-02-10 21:34:55 -0500

    Revert "Pre game Lobby"
    This reverts commit 814cfd5fd0d07dc6719a1bc5c5b4bf362d8609ad.

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -2,39 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement; 
 
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    public InputField usernameInput;
-    public Text buttonText;
-
     // Start is called before the first frame update
-    
-    // void Start()
-    // {
-    //     PhotonNetwork.ConnectUsingSettings();
-    // }
-    public void onClickConnect()
+    void Start()
     {
-        if (usernameInput.text.Length >= 1)
-        {
-            PhotonNetwork.NickName = usernameInput.text;
-            buttonText.text = " Connecting....";
-            PhotonNetwork.ConnectUsingSettings();
-        }
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // Update is called once per frame
     public override void OnConnectedToMaster()
     {
-        //PhotonNetwork.JoinLobby();
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
         SceneManager.LoadScene("Lobby");
     }
-    // public override void OnJoinedLobby()
-    // {
-    //     SceneManager.LoadScene("Lobby");
-    // }
 }
