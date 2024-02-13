ba41695ee0cdb907d53846e971eb7de0dbe6bb54
Vinit Jain <jain.vinit14@gmail.com>
2024-02-13 00:36:05 -0500

    changed each photon animation to discrete, added ismine code for animations

--- Assets/Scripts/SeaDragonMovement.cs
+++ Assets/Scripts/SeaDragonMovement.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
+using Photon.Pun;
 
-public class SeaDragonMovement : MonoBehaviour
+public class SeaDragonMovement : MonoBehaviourPunCallbacks
 {
     Animator dragonAnimator;
     Rigidbody dragonRigidBody;
@@ -14,27 +14,49 @@ public class SeaDragonMovement : MonoBehaviour
         dragonRigidBody = GetComponent<Rigidbody>();
 
         dragonAnimator.SetBool("Stand", true);
+
+        // Check if this is the local player's object
+        if (!photonView.IsMine)
+        {
+            // Disable control if this is not the local player's object
+            enabled = false;
+        }
     }
 
     void Update()
     {
+        if (!photonView.IsMine)
+            return;
+
         JumpUpdates();
         MovementUpdates();
         AttackUpdates();
     }
+
     void MovementUpdates()
     {
+        if (!photonView.IsMine)
+            return;
+
         dragonAnimator.SetBool("Stand", dragonAnimator.GetFloat("Vertical") == 0 && dragonAnimator.GetFloat("Horizontal") == 0);
         dragonAnimator.SetFloat("Vertical", SeaDragonMain.verticalInput);
         dragonAnimator.SetFloat("Horizontal", SeaDragonMain.horizontalInput);
     }
+
     void JumpUpdates()
     {
+        if (!photonView.IsMine)
+            return;
+
         dragonAnimator.SetBool("Jump", SeaDragonMain.isJumpKeyPressed);
         dragonAnimator.SetBool("Grounded", SeaDragonMain.isGrounded);
     }
+
     void AttackUpdates()
     {
+        if (!photonView.IsMine)
+            return;
+
         if (SeaDragonMain.isFire1Pressed)
         {
             dragonAnimator.SetBool("Attack2", true);
@@ -50,6 +72,5 @@ public class SeaDragonMovement : MonoBehaviour
             dragonAnimator.SetInteger("ActionID", -1);
             dragonAnimator.SetBool("Attack1", false);
         }
-
     }
 }
