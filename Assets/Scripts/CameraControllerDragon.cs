4f27654706974dd0b88f854c0eb10ca6df95814d
Vinit Jain <jain.vinit14@gmail.com>
2024-02-15 21:28:54 -0500

    fix for camera issues

--- Assets/Scripts/CameraControllerDragon.cs
+++ Assets/Scripts/CameraControllerDragon.cs
@@ -3,27 +3,19 @@ using Photon.Pun;
 
 public class CameraControllerDragon : MonoBehaviourPunCallbacks
 {
-    public GameObject cameraHolder;
-    public Vector3 offset;
+    [SerializeField] Camera dragonCam;
 
     void Start()
     {
-        if (!photonView.IsMine || photonView.Owner.ActorNumber != PhotonNetwork.LocalPlayer.ActorNumber)
+        if (!photonView.IsMine)
         {
             // Disable control if this is not the local player's camera
-            enabled = false;
+            Destroy(dragonCam);
         }
     }
 
     void Update()
     {
-        if (!photonView.IsMine || photonView.Owner.ActorNumber != PhotonNetwork.LocalPlayer.ActorNumber)
-            return;
 
-        Debug.Log("My Actor Number :" + photonView.Owner.ActorNumber);
-        Debug.Log("Other Actor Number :" + PhotonNetwork.LocalPlayer.ActorNumber);
-
-        // Move the camera holder to follow the local player's dragon with the offset
-        cameraHolder.transform.position = transform.position + offset;
     }
 }
