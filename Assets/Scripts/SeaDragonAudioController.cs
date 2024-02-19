df20d4658580db526ec0e0c11beb7493e72618ca
Vinit Jain <jain.vinit14@gmail.com>
2024-02-19 16:16:07 -0500

    working dragon fire sound with echo

--- Assets/Scripts/SeaDragonAudioController.cs
+++ Assets/Scripts/SeaDragonAudioController.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class SeaDragonAudioController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public AudioSource audioSource; // Single AudioSource
 
+    private Dictionary<string, AudioClip> sounds;
+    // Start is called before the first frame update
+
+
+    private void Start()
+    {
+        sounds = new Dictionary<string, AudioClip>();
+        sounds.Add("fire1", Resources.Load<AudioClip>("dragon fire breath"));
     }
+
+    public void PlaySound(string eventName)
+    {
+        if (sounds.TryGetValue(eventName, out AudioClip clip))
+        {
+            audioSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogWarning("Sound not found for event: " + eventName);
+        }
+    }
+    public void StopSound(string eventName)
+    {
+        audioSource.Stop();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (SeaDragonMain.isFire1Pressed)
         {
-            AudioSource[] audioSourceList = gameObject.GetComponents<AudioSource>();
-            for (int i = 0; i < audioSourceList.Length; i++)
-            {
-                Debug.Log(audioSourceList[i]);
-            }
-
+            PlaySound("fire1");
         }
     }
 }
